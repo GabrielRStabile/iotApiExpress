bd955de7eb7e9f1379b4627bb035e78f3058d0ad
guilhermeshibuya <gyudishibuya@gmail.com>
2024-05-16 16:58:10 -0300

    add: todas as rotas de eventType implementadas

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
+const eventTypeRouter = require('./routes/eventType');
 
 require('dotenv').config();
 
@@ -23,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
-
+app.use('/eventType', eventTypeRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
--- middleware/isAuthorized.js
+++ middleware/isAuthorized.js
@@ -3,10 +3,10 @@ const jwt = require('jsonwebtoken');
 const isAuthorized = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization) return res.status(403).json({ message: 'Empty token' });
+  if (!authorization) return res.status(403).json({ error: 'Token vazio' });
 
   jwt.verify(authorization, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.status(401).json({ message: 'Invalid token' });
+    if (err) return res.status(401).json({ error: 'Token inválido' });
 
     req.userId = decoded.id;
     return next();
