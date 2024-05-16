8c7728f780be755e60378f3b85d4f93e4219e2da
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2024-05-16 14:23:02 -0300

    chore: remove jade from project

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
@@ -19,15 +18,9 @@ mongoose
     console.log(err);
   });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
