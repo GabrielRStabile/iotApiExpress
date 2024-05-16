61b3ace9ca3b035d82cafb9968da8c5360884672
guilhermeshibuya <gyudishibuya@gmail.com>
2024-05-16 14:57:04 -0300

    add: verify if is authorized middleware

---
+++ middleware/isAuthorized.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const isAuthorized = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(403).json({ message: 'Empty token' });
+
+  jwt.verify(authorization, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) return res.status(401).json({ message: 'Invalid token' });
+
+    req.userId = decoded.id;
+    return next();
+  });
+};
